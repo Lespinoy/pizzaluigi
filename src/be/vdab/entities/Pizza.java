f05f9b647d650e2a685258bb36ee92f815c1e6ea
Michael <michael@lespinoy.be>
2017-09-18 14:15:13 +0200

    1.51

--- src/be/vdab/entities/Pizza.java
+++ src/be/vdab/entities/Pizza.java
@@ -7,60 +7,62 @@ public class Pizza {
 	private String naam;
 	private BigDecimal prijs;
 	private boolean pikant;
-	
+
 	public Pizza(String naam, BigDecimal prijs, boolean pikant) throws Exception {
 		setNaam(naam);
 		setPrijs(prijs);
 		setPikant(pikant);
 	}
-	
+
 	public Pizza(long id, String naam, BigDecimal prijs, boolean pikant) throws Exception {
 		this(naam, prijs, pikant);
 		setId(id);
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public void setPikant(boolean pikant) {
 		this.pikant = pikant;
 	}
-	
+
 	public void setNaam(String naam) throws Exception {
-		if (isNaamValid(naam)){
+		if (isNaamValid(naam)) {
 			this.naam = naam;
-		} else throw new IllegalArgumentException();
+		} else
+			throw new IllegalArgumentException();
 	}
-	
+
 	public void setPrijs(BigDecimal prijs) throws Exception {
-		if (isPrijsValid(prijs)){
+		if (isPrijsValid(prijs)) {
 			this.prijs = prijs;
-		} else throw new IllegalArgumentException();
+		} else
+			throw new IllegalArgumentException();
 	}
-		
-	public static boolean isPrijsValid(BigDecimal prijs){
-		return(prijs != null && prijs.compareTo(BigDecimal.ZERO) >= 0);
+
+	public static boolean isPrijsValid(BigDecimal prijs) {
+		return prijs != null && prijs.compareTo(BigDecimal.ZERO) >= 0;
 	}
-		
-	public static boolean isNaamValid(String naam){
+
+	public static boolean isNaamValid(String naam) {
 		return (naam != null && !naam.isEmpty());
 	}
-	
+
 	public boolean isPikant() {
 		return pikant;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public String getNaam() {
 		return naam;
 	}
-	
+
 	public BigDecimal getPrijs() {
 		return prijs;
 	}
-	
+
 }
--- src/be/vdab/servlets/PizzasServlet.java
+++ src/be/vdab/servlets/PizzasServlet.java
@@ -1,7 +1,11 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.vdab.entities.Pizza;
 import be.vdab.repositories.PizzaRepository;
 
 /**
@@ -20,16 +25,21 @@ public class PizzasServlet extends HttpServlet {
 	private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
 	private static final String PIZZA_REQUEST = "pizzaRequests";
 	private final PizzaRepository pizzaRepository = new PizzaRepository();
+	private String pizzaFotosPad;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		((AtomicInteger) this.getServletContext().getAttribute(PIZZA_REQUEST)).incrementAndGet();
-		request.setAttribute("pizzas", pizzaRepository.findAll());
+		List<Pizza> pizzas = pizzaRepository.findAll();
+		request.setAttribute("pizzas", pizzas);
+		request.setAttribute("pizzaIdsMetFoto",	pizzas.stream()
+		.filter(pizza -> Files.exists(Paths.get(pizzaFotosPad, pizza.getId() + ".jpg"))).map(pizza -> pizza.getId()).collect(Collectors.toList()));
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
-	
-	public void init() throws ServletException{
+
+	public void init() throws ServletException {
 		this.getServletContext().setAttribute(PIZZA_REQUEST, new AtomicInteger());
+		pizzaFotosPad = this.getServletContext().getRealPath("/pizzafotos");
 	}
 }
